99ebd3e632d842f3519a1c91c79beb4c3249d72a
tegarsetyo6 <tegarsetyo601@gmail.com>
2025-04-14 18:47:22 +0700

    remove history

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const indexRouter = require('./routes/admin/index/index.router');
 const indexAPIRouter = require('./routes/clients/index/index.router');
 const reportAPIRouter = require('./routes/clients/reports/reports.router');
 const authAPIRouter = require('./routes/clients/auth/auth.router');
-const historyAPIRouter = require('./routes/clients/history/history.controller');
 
 app.use('/', indexRouter);
 
